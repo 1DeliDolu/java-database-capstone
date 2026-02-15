b22fb7fd102f35c2f295e45a53fd15bc4a2adc9b
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2026-02-15 23:58:00 +0100

    ok

--- app/src/main/java/com/project/back_end/mvc/DashboardController.java
+++ app/src/main/java/com/project/back_end/mvc/DashboardController.java
@@ -1,6 +1,5 @@
 package com.project.back_end.mvc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +11,6 @@ public class DashboardController {
 
     private final Service service;
 
-    @Autowired
     public DashboardController(Service service) {
         this.service = service;
     }
